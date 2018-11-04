b1f9bc3648bdf0c0d6045cf28c36fdea8e6d332b
ZackAddy <77209302@qq.com>
2018-11-04 23:28:19 +0800

    no message

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
 module.exports = appInfo => {
-  const config = (exports = {})
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1541178168304_2532'
+  config.keys = appInfo.name + '_1541178168304_2532';
 
   // add your config here
-  config.middleware = []
-  return config
-}
+  config.middleware = [];
+  return config;
+};

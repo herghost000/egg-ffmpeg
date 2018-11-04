e0bfc148a3acc10af1e439dd9eab379b2d097b4c
herghost000 <77209302@qq.com>
2018-11-04 23:42:17 +0800

    commit

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,14 +1,14 @@
-'use strict'
-const Controller = require('egg').Controller
+'use strict';
+const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
     this.ctx.body = {
       name: 'egg',
       category: 'framework',
-      language: 'Node.js'
-    }
+      language: 'Node.js',
+    };
   }
 }
 
-module.exports = HomeController
+module.exports = HomeController;
--- app/router.js
+++ app/router.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app
-  router.get('/', controller.home.index)
-}
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
+};
